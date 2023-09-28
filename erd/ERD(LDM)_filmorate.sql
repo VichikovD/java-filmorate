6d50091874c5903a983947b90bc1286c575b7ea5
VichikovD <131587393+VichikovD@users.noreply.github.com>
2023-09-28 16:33:39 +0300

    Update README.md

--- erd/ERD(LDM)_filmorate.sql
+++ erd/ERD(LDM)_filmorate.sql
@@ -12,12 +12,30 @@ CREATE TABLE "films" (
   "description" varchar(200) NOT NULL,
   "releaseDate" date NOT NULL,
   "duration" integer NOT NULL,
+  "rating_id" integer NOT NULL,
   "likesQuantity" integer
+);
+
+CREATE TABLE "genres" (
+  "genre_id" integer PRIMARY KEY,
+  "genre_name" varchar NOT NULL
+);
+
+CREATE TABLE "ratings" (
+  "rating_id" integer PRIMARY KEY,
+  "rating_name" varchar NOT NULL
+);
+
+CREATE TABLE "films_genres" (
+  "film_id" integer,
+  "genre_id" integer,
+  PRIMARY KEY ("film_id", "genre_id")
 );
 
 CREATE TABLE "friends" (
   "user_id" integer,
   "friend_id" integer,
+  "is_approved" boolean NOT NULL,
   PRIMARY KEY ("user_id", "friend_id")
 );
 
@@ -40,3 +58,9 @@ ALTER TABLE "friends" ADD FOREIGN KEY ("friend_id") REFERENCES "users" ("user_id
 ALTER TABLE "likes" ADD FOREIGN KEY ("film_id") REFERENCES "films" ("film_id");
 
 ALTER TABLE "likes" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("user_id");
+
+ALTER TABLE "films_genres" ADD FOREIGN KEY ("film_id") REFERENCES "films" ("film_id");
+
+ALTER TABLE "films_genres" ADD FOREIGN KEY ("genre_id") REFERENCES "genres" ("genre_id");
+
+ALTER TABLE "films" ADD FOREIGN KEY ("rating_id") REFERENCES "ratings" ("rating_id");
