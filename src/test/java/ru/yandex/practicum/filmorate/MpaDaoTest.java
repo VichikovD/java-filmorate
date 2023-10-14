eaab1c5a832bd0530be8671c106b69e5a9fa743e
D.Vichikov <Rodderh@yandex.ru>
2023-10-14 17:08:05 +0200

    Pull request #3; Style: style corrections;

--- src/test/java/ru/yandex/practicum/filmorate/MpaDaoTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/MpaDaoTest.java
@@ -36,17 +36,17 @@ public class MpaDaoTest {
     @Test
     public void testGetById() {
         List<Mpa> mpas = data.getMpaList();
-        Mpa Mpa1 = mpaDao.getById(1).get();
-        Mpa Mpa2 = mpaDao.getById(2).get();
-        Mpa Mpa3 = mpaDao.getById(3).get();
-        Mpa Mpa4 = mpaDao.getById(4).get();
-        Mpa Mpa5 = mpaDao.getById(5).get();
+        Mpa mpa1 = mpaDao.getById(1).get();
+        Mpa mpa2 = mpaDao.getById(2).get();
+        Mpa mpa3 = mpaDao.getById(3).get();
+        Mpa mpa4 = mpaDao.getById(4).get();
+        Mpa mpa5 = mpaDao.getById(5).get();
 
-        assertEquals(mpas.get(0), Mpa1);
-        assertEquals(mpas.get(1), Mpa2);
-        assertEquals(mpas.get(2), Mpa3);
-        assertEquals(mpas.get(3), Mpa4);
-        assertEquals(mpas.get(4), Mpa5);
+        assertEquals(mpas.get(0), mpa1);
+        assertEquals(mpas.get(1), mpa2);
+        assertEquals(mpas.get(2), mpa3);
+        assertEquals(mpas.get(3), mpa4);
+        assertEquals(mpas.get(4), mpa5);
         assertThrows(NoSuchElementException.class, () -> {
             mpaDao.getById(6).get();
         });
