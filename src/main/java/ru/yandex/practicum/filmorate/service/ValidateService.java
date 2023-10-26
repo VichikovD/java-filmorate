2ef3eb4adbd33ef505e28859e514fc3f01c39474
D.Vichikov <Rodderh@yandex.ru>
2023-10-26 18:25:50 +0300

    feat: review to film from users; likes/dislikes to review; crud operations for review

--- src/main/java/ru/yandex/practicum/filmorate/service/ValidateService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/ValidateService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.InvalidIdException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Review;
 import ru.yandex.practicum.filmorate.model.User;
 
 @Service
@@ -23,6 +24,12 @@ public class ValidateService {
     public void validateUserId(User user) {
         if (user.getId() == null) {
             throw new InvalidIdException("Id should not be empty");
+        }
+    }
+
+    public void validateReviewId(Review review) {
+        if (review.getReviewId() == null) {
+            throw new InvalidIdException("reviewId should not be empty");
         }
     }
 }
