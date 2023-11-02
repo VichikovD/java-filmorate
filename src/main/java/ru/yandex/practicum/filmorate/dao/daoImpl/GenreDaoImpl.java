392062ad3b84c0cadd65e503cfce167d11f589b3
Druzhinin Yury <60572414+druzhininyury@users.noreply.github.com>
2023-11-02 11:26:43 +0300

    fix: replace JdbcTemplate with NamedParameterJdbcTemplate. (#41)

--- src/main/java/ru/yandex/practicum/filmorate/dao/daoImpl/GenreDaoImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/daoImpl/GenreDaoImpl.java
@@ -2,6 +2,9 @@ package ru.yandex.practicum.filmorate.dao.daoImpl;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.support.rowset.ResultSetWrappingSqlRowSet;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
@@ -15,11 +18,11 @@ import java.util.Optional;
 @Slf4j
 @Component
 public class GenreDaoImpl implements GenreDao {
-    JdbcTemplate jdbcTemplate;
+    NamedParameterJdbcTemplate namedParameterJdbcTemplate;
     ValidateService validateService;
 
-    public GenreDaoImpl(JdbcTemplate jdbcTemplate, ValidateService validateService) {
-        this.jdbcTemplate = jdbcTemplate;
+    public GenreDaoImpl(NamedParameterJdbcTemplate namedParameterJdbcTemplate, ValidateService validateService) {
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
         this.validateService = validateService;
     }
 
@@ -29,16 +32,17 @@ public class GenreDaoImpl implements GenreDao {
                 "FROM genres " +
                 "ORDER BY genre_id";
 
-        return jdbcTemplate.query(sql, (rs, rowNum) -> makeGenre(new ResultSetWrappingSqlRowSet(rs)));
+        return namedParameterJdbcTemplate.query(sql, (rs, rowNum) -> makeGenre(new ResultSetWrappingSqlRowSet(rs)));
     }
 
     @Override
     public Optional<Genre> getById(int genreId) {
         String sqlSelect = "SELECT genre_id, genre_name " +
                 "FROM genres " +
-                "WHERE genre_id = ?";
+                "WHERE genre_id = :genre_id";
 
-        SqlRowSet rsGenre = jdbcTemplate.queryForRowSet(sqlSelect, genreId);
+        SqlParameterSource parameters = new MapSqlParameterSource("genre_id", genreId);
+        SqlRowSet rsGenre = namedParameterJdbcTemplate.queryForRowSet(sqlSelect, parameters);
         if (rsGenre.next()) {
             Genre genre = makeGenre(rsGenre);
             return Optional.of(genre);
